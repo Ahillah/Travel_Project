442158b6d70a15003782d3e36a196ba30047a43c
Ahillah <90210728+Ahillah@users.noreply.github.com>
2025-12-16 22:33:00 +0200

    Flight

---
+++ Persistance/Migrations/20251207185254_UpdateFlight.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistance.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateFlight : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "TotalCapacity",
+                table: "Flights",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalCapacity",
+                table: "Flights");
+        }
+    }
+}
